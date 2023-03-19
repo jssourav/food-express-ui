cf349cf6a7a8596a34e2d5778b8953f9222332a3
Sourav Nag <sn.sourav1993@gmail.com>
2023-03-19 06:01:31 +0530

    Revert "build test 2"
    This reverts commit 7842202d465f1e2937a3ddf2853e5a786900977c.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./Components/admin/Dashboard";
+import SignIn from "./Components/admin/SignIn";
+import Body from "./Components/Body";
+import Home from "./Components/Home";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "signin",
+        element: <SignIn />,
+      },
+    ],
+  },
+  {
+    path: "admin",
+    element: <Dashboard />,
+  },
+]);
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Home />
     </>
   );
 }
 
-export default App;
+export default appRouter;
