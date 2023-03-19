7842202d465f1e2937a3ddf2853e5a786900977c
Sourav Nag <sn.sourav1993@gmail.com>
2023-03-19 05:57:02 +0530

    build test 2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import { createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import Dashboard from "./Components/admin/Dashboard";
-import SignIn from "./Components/admin/SignIn";
-import Body from "./Components/Body";
-import Home from "./Components/Home";
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Body />,
-      },
-      {
-        path: "signin",
-        element: <SignIn />,
-      },
-    ],
-  },
-  {
-    path: "admin",
-    element: <Dashboard />,
-  },
-]);
 
 function App() {
   return (
     <>
-      <Home />
+      <h1>Hello</h1>
     </>
   );
 }
 
-export default appRouter;
+export default App;
